fb9bb39e08ec9df27c53a373489a5457686cff65
chloepei867 <pei.zhiy@northeastern.edu>
2025-09-20 23:53:53 -0700

    completed User class

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+import { User } from "./models/User";
+
+// axios.post("http://localhost:3000/users", {
+//   id: 123,
+//   name: "alice",
+//   age: 34,
+// });
+
+// axios
+//   .get("http://localhost:3000/posts/1")
+//   .then((response: AxiosResponse): void => {
+//     console.log(response.data);
+//   });
+
+// const user = new User({ id: 123 });
+// user.fetch();
+
+// setTimeout(() => {
+//   console.log(user);
+// }, 4000);
+
+const newUser: User = new User({
+  name: "clair",
+  age: 111,
+});
+
+newUser.save();
